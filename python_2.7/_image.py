e5faf3c52330b9d14d21f8086daa1b6a80b86abf
fpsaandnoob <159199827@qq.com>
2017-04-24 15:58:57 +0800

    resize

--- python_2.7/_image.py
+++ python_2.7/_image.py
@@ -18,20 +18,20 @@ def work(image_name, x1, y1, x2, y2, class_name):
     im = Image.open(image_dir + image_name[7:])
     box = (x1, y1, x2, y2)
     region = im.crop(box)
-    region = region.resize((256, 256))
+    region = region.resize((224, 224))
     region.save(output_dir + car_name['class_names'][0][int(class_name) - 1][0] + image_name[7:])
 
 
 def create_dir():
     for i in range(195):
-        if not os.path.exists(output_dir + car_name['class_names'][0][i + 1][0]):
-            os.makedirs(output_dir + car_name['class_names'][0][i + 1][0])
+        if not os.path.exists(output_dir + car_name['class_names'][0][i][0]):
+            os.makedirs(output_dir + car_name['class_names'][0][i][0])
 
 
 if __name__ == '__main__':
     create_dir()
     for i in range(16184):
-        print (str(i/16184) + '%')
+        print i
         work(car_data['annotations'][0][i][0][0],
              car_data['annotations'][0][i][1][0],
              car_data['annotations'][0][i][2][0],
